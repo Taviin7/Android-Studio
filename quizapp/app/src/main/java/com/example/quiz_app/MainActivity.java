a23b02055ce7ee572748d11e32fb9e397eed3ee6
Taviin7 <otaviorl711@gmail.com>
2026-03-12 22:04:08 -0300

    Subindo Quiz App

---
+++ quizapp/app/src/main/java/com/example/quiz_app/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.quiz_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btn_java = findViewById(R.id.btn_java);
+        Button btn_sql = findViewById(R.id.btn_sql);
+        Button btn_so = findViewById(R.id.btn_so);
+
+        btn_java.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, QuestionsJavaActivity.class);
+                startActivity(i);
+                //finish();
+            }
+        });
+
+        btn_sql.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, QuestionsSqlActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btn_so.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, QuestionsSOActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
